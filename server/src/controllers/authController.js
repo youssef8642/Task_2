df7bd62ff272e06fb2ba255c1a74f5a96525286d
youssef8642 <youssef.mahmoudsaleh@student.guc.edu.eg>
2025-10-16 09:35:55 +0300

    my comit

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -29,9 +29,22 @@ const loginSchema = Joi.object({
   password: Joi.string().required()
 });
 
-// TODO: implement login function
 export async function login(req, res, next) {
- 
+  try {
+    const { value, error } = loginSchema.validate(req.body);
+    if (error) return res.status(400).json({ message: error.message });
+
+    const user = await User.findOne({ email: value.email });
+    if (!user) return res.status(401).json({ message: 'Invalid email or password' });
+
+    const ok = await user.comparePassword(value.password);
+    if (!ok) return res.status(401).json({ message: 'Invalid email or password' });
+
+    const token = signToken(user);
+    res.json({ token, user: publicUser(user) });
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function me(req, res) {
